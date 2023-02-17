fd93339a1f6c55096ed19a61e56deb223081f45e
Pappiburr <116762210+Pappiburr@users.noreply.github.com>
2023-02-16 22:04:27 -0600

    Button Disabling, second video up
    Made it to where both buttons disable once one  is chose. Videos now show underneath with blank space on either side. Next update, should add window to move forward on z-index to allow for arrows move up along the box.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+// Count Down Video //
+
 $('#countdown').on('click', function(){
 
+    $('#countdown-vid').css('visibility', 'visible');
     $('#countdown-vid').trigger('play');
     $('#countdown-vid').attr("loop","loop");
-    
-    
-
-    const audioCD = new Audio("sounds/Rex Banner - Countdown.mp3");
+    $('#countdown').attr('disabled', 'true');
+    $('#special-vibes').attr('disabled', 'true');
+    $('#special-vid').css('z-index', '-1');
+    var audioCD = new Audio("sounds/Rex Banner - Countdown.mp3");
     audioCD.play();
 
 });
+
+            // Vibe Video and Audio// 
 $('#special-vibes').on('click', function(){
-    alert("i've also been clicked");
+
+    $('#special-vid').css('visibility', 'visible');
+    $('#special-vid').trigger('play');
+    $('#special-vid').attr("loop","loop");
+    $('#countdown').attr('disabled', 'true');
+    $('#special-vibes').attr('disabled', 'true');
+    
+
+    var audioSV = new Audio("sounds/Yarin Primak - Special Vibe.mp3");
+    audioSV.play();
 });
