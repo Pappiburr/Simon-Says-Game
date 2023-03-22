c4e2ab5a2e0e7ae93488ea393f28ce9079010eee
Pappiburr <divinewind42@gmail.com>
2023-03-22 16:16:01 -0500

    LoopityLoop Function
    Created function that is a series of timeouts that will activate the series of animations then clear them.  basic design principle, not sure if i want to go more into it

--- index.js
+++ index.js
@@ -47,43 +47,74 @@ setTimeout(function(){
 var audioSV = new Audio("sounds/Yarin Primak - Special Vibe.mp3");
             // Vibe Video and Audio// 
             $('#special-vibes').on('click', function(){
-                let time3 = setTimeout(function(){$('#intro').html('3');},500) 
-                let time2 = setTimeout(function(){$('#intro').html('2');},1000) 
-                let time1 = setTimeout(function(){$('#intro').html('1');},1500) 
-                let timeGO = setTimeout(function(){$('#intro').html('GO!');},2000)
-                let specialVidStart = setTimeout(function(){$('#special-vid').css('visibility', 'visible');},2500)
-                let SpecialGamebarStart = setTimeout(function(){$('#gamebar1').css('visibility','visible');},2500)
+                setTimeout(function(){$('#intro').html('3');},500) 
+                setTimeout(function(){$('#intro').html('2');},1000) 
+                setTimeout(function(){$('#intro').html('1');},1500) 
+                setTimeout(function(){$('#intro').html('GO!');},2000)
+                setTimeout(function(){$('#special-vid').css('visibility', 'visible');},2500)
+                setTimeout(function(){$('#gamebar1').css('visibility','visible');},2500)
             
             
                 $('#special-vid').trigger('play');
                 $('#special-vid').attr("loop","loop");
                 $('#countdown').attr('disabled', 'true');
                 $('#special-vibes').attr('disabled', 'true');
-            audioSV.play();
+                    audioSV.play();
+                         function isPlaying(audioSV) { return !audioSV.paused; }
+                    loopityLoop();
+
+
+                //     while (isPlaying(audioSV)===true){
+                //         loopityLoop();
+                
+                // }
             });
             function isPlaying(audioSV) { return !audioSV.paused; }
 
-// $('#special-vibes').on('click', function(){
-//     setTimeout(function(){$('#topArrowMove').animate({'top':'285',}, 2000 );},2300)
-//     setTimeout(function(){$('#rightArrowMove').animate({'top':'285',}, 2000 );},2850)
-//     setTimeout(function(){$('#downArrowMove').animate({'top':'285',}, 2000 );},3000)
-//     setTimeout(function(){$('#leftArrowMove').animate({'top':'285',}, 2000 );},2650)
-// })
-function arrowAnimate(){
-    setInterval(function(){$('#topArrowMove').animate({'top':'285',}, 2500 );},2300)
-    setInterval(function(){$('#rightArrowMove').animate({'top':'285',}, 2500 );},2850)
-    setInterval(function(){$('#downArrowMove').animate({'top':'285',}, 2500 );},3000)
-    setInterval(function(){$('#leftArrowMove').animate({'top':'285',}, 2500 );},2650)
+function arrowAnimate1(){
+    $('#topArrowMove').animate({'top':'285',}, 2400 );
+    $('#rightArrowMove').animate({'top':'285',}, 2000 );
+    $('#downArrowMove').animate({'top':'285',}, 2600 );
+    $('#leftArrowMove').animate({'top':'285',}, 1900 );
 }
-
-while (isPlaying(audioSV)===true){
-        arrowAnimate();
-
+function arrowAnimate2(){
+    $('#topArrowMove').animate({'top':'285',}, 1600 );
+    $('#rightArrowMove').animate({'top':'285',}, 2400 );
+    $('#downArrowMove').animate({'top':'285',}, 2700 );
+    $('#leftArrowMove').animate({'top':'285',}, 2000 );
 }
+function arrowAnimate3(){
+    $('#topArrowMove').animate({'top':'285',}, 1950 );
+    $('#rightArrowMove').animate({'top':'285',}, 2200 );
+    $('#downArrowMove').animate({'top':'285',}, 2800 );
+    $('#leftArrowMove').animate({'top':'285',}, 1500 );
+}
+function arrowClear(){
+    $('#topArrowMove').css('top', '1250px');
+    $('#rightArrowMove').css('top', '1250px');
+    $('#downArrowMove').css('top', '1250px');
+    $('#leftArrowMove').css('top', '1250px');
+}
+function loopityLoop(){
+    setTimeout(arrowAnimate2, 3000)
+    setTimeout(arrowClear, 6100)
+    setTimeout(arrowAnimate3, 6150)
+    setTimeout(arrowClear, 9200)
+    setTimeout(arrowAnimate1,9350 )
+    setTimeout(arrowClear, 12350)
+    setTimeout(arrowAnimate3, 12400)
+    setTimeout(arrowClear, 15300)
+    setTimeout(arrowAnimate2, 15350)
+    setTimeout(arrowClear, 18500)
+    setTimeout(arrowAnimate1, 18550)
+    setTimeout(arrowClear, 21550)
+    setTimeout(arrowAnimate2,21600 )
+    setTimeout(arrowClear, 24650)
+    setTimeout(arrowAnimate3, 24700)
+    setTimeout(arrowClear, 27750)
 
-// function arrowAnimate(){
-//     setTimeout(function(){$('#topArrowMove').animate({'top':'285',}, 2000 );},2300)
-//     setTimeout(function(){$('#rightArrowMove').animate({'top':'285',}, 2000 );},2850)
-//     setTimeout(function(){$('#downArrowMove').animate({'top':'285',}, 2000 );},3000)
-//     setTimeout(function(){$('#leftArrowMove').animate({'top':'285',}, 2000 );},2650)
-// }
+    
+}   
+
+
+
